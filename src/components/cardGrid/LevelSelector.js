c8731e3cc8c37a2a7cc1da4731a73186d939a433
AxelTur-Springer <axel.turspringer@gmail.com>
2022-05-26 17:21:52 -0300

    FIX

--- src/components/cardGrid/LevelSelector.js
+++ src/components/cardGrid/LevelSelector.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function LevelComp(props){
     return(
         <div>
-            <select onClick= {props.SelectLevel}>
+            <select onChange= {props.SelectLevel}>
                 <option value="Select Dificulty">Select Dificulty</option>
                 <option value="Easy">Easy</option>
                 <option value="Medium">Medium</option>
